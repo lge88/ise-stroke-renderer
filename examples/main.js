8d180230251ccb58e14b03076ca03f8ee28646cb
Li Ge <lge@ucsd.edu>
2013-10-03 13:04:30 -0700

    Update at Thu Oct  3 13:04:30 PDT 2013

---
+++ examples/main.js
@@ -0,0 +1,16 @@
+
+var arrgen = require( 'arr-gen' );
+var StrokeRenderer = require( 'ise-stroke-renderer' );
+var can = document.getElementById( 'main' );
+
+var strokeRenderer = new StrokeRenderer( can );
+
+var s1 = arrgen( 100, function( i ) {
+  return {
+    x: i,
+    y: i
+  };
+} );
+
+id = strokeRenderer.add( s1 );
+strokeRenderer.render();
---
+++ test/test.js
@@ -0,0 +1,11 @@
+
+var lib = require( 'ise-stroke-renderer' );
+var expect = require( 'expect.js' );
+
+describe( 'ise-stroke-renderer', function() {
+
+  it( 'should return Hello World', function() {
+    expect( lib() ).to.be( 'Hello World' );
+  } );
+
+} );
